ff4c05894cfc20c1fb2c7f37081dc69ca9cf6ac2
desonglll <lindesong666@163.com>
2025-05-13 09:47:30 +0800

    First Commit.

---
+++ src/main/java/com/sd/alohaspring/AlohaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sd.alohaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlohaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlohaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sd/alohaspring/AlohaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd.alohaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlohaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
