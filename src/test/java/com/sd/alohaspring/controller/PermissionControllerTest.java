f7753b50b7fbf90979712c60ac93c5cafb7e1119
desonglll <lindesong666@163.com>
2025-05-14 15:05:18 +0800

    Fix test configurations.

--- src/test/java/com/sd/alohaspring/controller/PermissionControllerTest.java
+++
@@ -1,8 +0,0 @@
-package com.sd.alohaspring.controller;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-@WebMvcTest(PermissionController.class)
-public class PermissionControllerTest {
-
-}
