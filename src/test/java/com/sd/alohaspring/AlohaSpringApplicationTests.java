4f96d594949d549642d738122229053bda4cefb2
desonglll <lindesong666@163.com>
2025-05-14 15:33:45 +0800

    Fix test configurations.

--- src/test/java/com/sd/alohaspring/AlohaSpringApplicationTests.java
+++ src/test/java/com/sd/alohaspring/AlohaSpringApplicationTests.java
@@ -2,12 +2,14 @@ package com.sd.alohaspring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AlohaSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
