439e47d38a31c2964abb09c5ee27dd48b1af98ef
desonglll <lindesong666@163.com>
2025-05-16 22:49:26 +0800

    Update CRUD functions.

--- src/main/java/com/sd/alohaspring/WebConfig.java
+++ src/main/java/com/sd/alohaspring/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 允许所有接口
                         .allowedOrigins("http://localhost:5173") // 前端地址
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
